054bdc42b89a67744bf6c29f30e67d9af1bbea48
Kelvin He <h.muyuan@wustl.edu>
2025-10-01 13:43:14 -0500

    Washu Flag completed

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,52 @@
 package studio4;
 
+import edu.princeton.cs.introcs.StdDraw;
 import java.awt.Color;
 
-import edu.princeton.cs.introcs.StdDraw;
-
 public class Flag {
-	public static void main(String[] args) {
-		
-	}
+    public static void main(String[] args) {
+        StdDraw.setCanvasSize(900, 600);
+        StdDraw.setXscale(0, 1.5);
+        StdDraw.setYscale(0, 1);
+
+        Color washuRed   = new Color(186, 12, 47);
+        Color washuGreen = new Color(0, 97, 63);
+        Color white      = new Color(255, 255, 255);
+
+        StdDraw.setPenColor(washuRed);
+        StdDraw.filledRectangle(0.75, 0.833, 0.75, 0.167);
+        StdDraw.setPenColor(white);
+        StdDraw.filledRectangle(0.75, 0.5, 0.75, 0.167);
+        StdDraw.setPenColor(washuGreen);
+        StdDraw.filledRectangle(0.75, 0.167, 0.75, 0.167);
+
+        StdDraw.setPenColor(Color.BLACK);
+        StdDraw.line(0, 0.667, 1.5, 0.667);
+        StdDraw.line(0, 0.333, 1.5, 0.333);
+
+        StdDraw.setPenRadius(0.005);
+        StdDraw.rectangle(0.75, 0.5, 0.75, 0.5);
+        StdDraw.setPenRadius();
+
+        drawBear(0.75, 0.5, 0.6, washuGreen, white);
+    }
+
+    public static void drawBear(double cx, double cy, double scale, Color bearColor, Color accent) {
+        StdDraw.setPenColor(bearColor);
+        StdDraw.filledEllipse(cx, cy, 0.35*scale, 0.18*scale);
+        StdDraw.filledCircle(cx + 0.38*scale, cy + 0.05*scale, 0.12*scale);
+        StdDraw.filledCircle(cx + 0.32*scale, cy + 0.16*scale, 0.04*scale);
+        StdDraw.filledCircle(cx + 0.44*scale, cy + 0.14*scale, 0.04*scale);
+        StdDraw.filledRectangle(cx - 0.25*scale, cy - 0.18*scale, 0.05*scale, 0.12*scale);
+        StdDraw.filledRectangle(cx - 0.05*scale, cy - 0.18*scale, 0.05*scale, 0.12*scale);
+        StdDraw.filledRectangle(cx + 0.15*scale, cy - 0.18*scale, 0.05*scale, 0.12*scale);
+        StdDraw.filledRectangle(cx + 0.35*scale, cy - 0.18*scale, 0.05*scale, 0.12*scale);
+        StdDraw.filledCircle(cx - 0.38*scale, cy + 0.05*scale, 0.05*scale);
+        StdDraw.setPenColor(accent);
+        StdDraw.filledEllipse(cx + 0.45*scale, cy + 0.02*scale, 0.05*scale, 0.03*scale);
+        StdDraw.setPenColor(bearColor);
+        StdDraw.filledCircle(cx + 0.49*scale, cy + 0.02*scale, 0.015*scale);
+        StdDraw.setPenColor(accent);
+        StdDraw.filledCircle(cx + 0.42*scale, cy + 0.07*scale, 0.012*scale);
+    }
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -5,20 +5,72 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 import javax.swing.JFileChooser;
-import javax.swing.SwingUtilities;
 
+import java.awt.Color;
 import edu.princeton.cs.introcs.StdDraw;
 
-/**
- * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
- */
 public class InterpretDrawingFile {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		JFileChooser chooser = new JFileChooser("resources");
-		chooser.showOpenDialog(null);
-		File f = new File(chooser.getSelectedFile().getPath());
-		Scanner in = new Scanner(f); //making Scanner with a File
-		
-	}
+    public static void main(String[] args) throws FileNotFoundException {
+        // Open file chooser starting in resources folder
+        JFileChooser chooser = new JFileChooser("resources");
+        chooser.showOpenDialog(null);
+        File f = chooser.getSelectedFile();
+        Scanner in = new Scanner(f); // Scanner attached to chosen file
+
+        while (in.hasNext()) {
+            // common values
+            String shapeType = in.next();
+            int r = in.nextInt();
+            int g = in.nextInt();
+            int b = in.nextInt();
+            boolean isFilled = in.nextBoolean();
+
+            StdDraw.setPenColor(new Color(r, g, b));
+
+            if (shapeType.equals("Rectangle")) {
+                double x = in.nextDouble();
+                double y = in.nextDouble();
+                double halfW = in.nextDouble();
+                double halfH = in.nextDouble();
+
+                if (isFilled) {
+                    StdDraw.filledRectangle(x, y, halfW, halfH);
+                } else {
+                    StdDraw.rectangle(x, y, halfW, halfH);
+                }
+
+            } else if (shapeType.equals("Ellipse")) {
+                double x = in.nextDouble();
+                double y = in.nextDouble();
+                double halfW = in.nextDouble();
+                double halfH = in.nextDouble();
+
+                if (isFilled) {
+                    StdDraw.filledEllipse(x, y, halfW, halfH);
+                } else {
+                    StdDraw.ellipse(x, y, halfW, halfH);
+                }
+
+            } else if (shapeType.equals("Triangle")) {
+                double x1 = in.nextDouble();
+                double y1 = in.nextDouble();
+                double x2 = in.nextDouble();
+                double y2 = in.nextDouble();
+                double x3 = in.nextDouble();
+                double y3 = in.nextDouble();
+
+                double[] xs = {x1, x2, x3};
+                double[] ys = {y1, y2, y3};
+
+                if (isFilled) {
+                    StdDraw.filledPolygon(xs, ys);
+                } else {
+                    StdDraw.polygon(xs, ys);
+                }
+            }
+        }
+
+        in.close();
+    }
 }
